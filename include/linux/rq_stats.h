d2d6b0ffb881e5300a1e28c86ab712289586d055
desalesouche <desalesouche@gmail.com>
2014-09-17 13:35:08 +0200

    first commit

---
+++ include/linux/rq_stats.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+/* moved to arch/arm/mach-msm/msm_rq_stats.c */
+
+#ifdef CONFIG_MSM_RUN_QUEUE_STATS
+extern void msm_update_rq_stats(void);
+#endif
